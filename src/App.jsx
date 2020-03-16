098c8f4881e3807933a41f527c6d5c81d23c93b5
sanshi1929 <sanshi2929@gmail.com>
2020-03-17 04:03:05 +0900

    'antd'

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Button, message } from 'antd';
+
+export default class App extends Component {
+
+    handleClick = () => {
+        message.success('成功了。。。')
+    }
+    render() {
+        return (
+            <Button type="primary" onClick={this.handleClick}>Primary</Button>
+        )
+    }
+}
