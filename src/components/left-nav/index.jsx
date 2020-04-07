df6c3ec8ed5aadf0deb48d0ba385fbbeeca05871
sanshi1929 <sanshi2929@gmail.com>
2020-04-08 01:17:21 +0900

    'user'

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -1,114 +1,203 @@
-import React, { Component } from "react";
-import { Link, withRouter } from "react-router-dom";
-import "./index.css";
-import logo from "../../assets/images/logo.png";
-import { Menu, Icon, Button } from "antd";
-import menuList from "../../config/menuConfig";
-const { SubMenu } = Menu;
+import React, { Component } from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import { Menu, Icon } from 'antd';
 
+import logo from '../../assets/images/logo.png'
+import menuList from '../../config/menuConfig'
+import './index.less'
+import memoryUtils from "../../utils/memoryUtils";
+
+const SubMenu = Menu.SubMenu;
+
+/*
+左侧导航的组件
+ */
 class LeftNav extends Component {
-  //map方法
-  getMenuNodes_map = menuList => {
-    return menuList.map(item => {
-      if (!item.children) {
-        return (
-          <Menu.Item key={item.key}>
-            <Link to={item.key}>
-              <Icon type={item.icon} />
-              <span>{item.title}</span>
-            </Link>
-          </Menu.Item>
-        );
-      } else {
-        return (
-          <SubMenu
-            key={item.key}
-            title={
-              <span>
-                <Icon type={item.icon} />
-                <span>{item.title}</span>
-              </span>
-            }
-          >
-            {this.getMenuNodes(item.children)}
-          </SubMenu>
-        );
-      }
-    });
-  };
-  //reduce方法
-  getMenuNodes = menuList => {
-    // 得到当前请求的path
-    const path = this.props.location.pathname;
-    return menuList.reduce((pre, item) => {
-      if (!item.children) {
-        pre.push(
-          <Menu.Item key={item.key}>
-            <Link to={item.key}>
-              <Icon type={item.icon} />
-              <span>{item.title}</span>
-            </Link>
-          </Menu.Item>
-        );
-      } else {
-        // 如果当前请求路由与当前菜单的某个子菜单的key 匹配, 将菜单的key 保存为openKey
-        // if (item.children.find(cItem => path.indexOf(cItem.key) === 0)) {
-        //     this.openKey = item.key
-        // }
-        const cItem = item.children.find(cItem => cItem.key === path);
-        if (cItem) {
-          this.openKey = item.key;
-        }
-        pre.push(
-          <SubMenu
-            key={item.key}
-            title={
-              <span>
-                <Icon type={item.icon} />
-                <span>{item.title}</span>
-              </span>
-            }
-          >
-            {this.getMenuNodes(item.children)}
-          </SubMenu>
-        );
-      }
-      return pre;
-    }, []);
-  };
+
   /*
-    在第一次render()之前执行一次
-    一般可以在此同步为第一次render()准备数据
-    */
-  componentWillMount() {
-    this.menuNodes = this.getMenuNodes(menuList);
+  判断当前登陆用户对item是否有权限
+   */
+  hasAuth = (item) => {
+    const { key, isPublic } = item
+
+    const menus = memoryUtils.user.role.menus
+    const username = memoryUtils.user.username
+    /*
+    1. 如果当前用户是admin
+    2. 如果当前item是公开的
+    3. 当前用户有此item的权限: key有没有menus中
+     */
+    if (username === 'admin' || isPublic || menus.indexOf(key) !== -1) {
+      return true
+    } else if (item.children) { // 4. 如果当前用户有此item的某个子item的权限
+      return !!item.children.find(child => menus.indexOf(child.key) !== -1)
+    }
+
+    return false
   }
+
+  /*
+  根据menu的数据数组生成对应的标签数组
+  使用map() + 递归调用
+  */
+  getMenuNodes_map = (menuList) => {
+    return menuList.map(item => {
+      /*
+        {
+          title: '首页', // 菜单标题名称
+          key: '/home', // 对应的path
+          icon: 'home', // 图标名称
+          children: [], // 可能有, 也可能没有
+        }
+
+        <Menu.Item key="/home">
+          <Link to='/home'>
+            <Icon type="pie-chart"/>
+            <span>首页</span>
+          </Link>
+        </Menu.Item>
+
+        <SubMenu
+          key="sub1"
+          title={
+            <span>
+              <Icon type="mail"/>
+              <span>商品</span>
+            </span>
+          }
+        >
+          <Menu.Item/>
+          <Menu.Item/>
+        </SubMenu>
+      */
+      if (!item.children) {
+        return (
+          <Menu.Item key={item.key}>
+            <Link to={item.key}>
+              <Icon type={item.icon} />
+              <span>{item.title}</span>
+            </Link>
+          </Menu.Item>
+        )
+      } else {
+        return (
+          <SubMenu
+            key={item.key}
+            title={
+              <span>
+                <Icon type={item.icon} />
+                <span>{item.title}</span>
+              </span>
+            }
+          >
+            {this.getMenuNodes(item.children)}
+          </SubMenu>
+        )
+      }
+
+    })
+  }
+
+  /*
+  根据menu的数据数组生成对应的标签数组
+  使用reduce() + 递归调用
+  */
+  getMenuNodes = (menuList) => {
+    // 得到当前请求的路由路径
+    const path = this.props.location.pathname
+
+    return menuList.reduce((pre, item) => {
+
+      // 如果当前用户有item对应的权限, 才需要显示对应的菜单项
+      if (this.hasAuth(item)) {
+        // 向pre添加<Menu.Item>
+        if (!item.children) {
+          pre.push((
+            <Menu.Item key={item.key}>
+              <Link to={item.key}>
+                <Icon type={item.icon} />
+                <span>{item.title}</span>
+              </Link>
+            </Menu.Item>
+          ))
+        } else {
+
+          // 查找一个与当前请求路径匹配的子Item
+          const cItem = item.children.find(cItem => path.indexOf(cItem.key) === 0)
+          // 如果存在, 说明当前item的子列表需要打开
+          if (cItem) {
+            this.openKey = item.key
+          }
+
+
+          // 向pre添加<SubMenu>
+          pre.push((
+            <SubMenu
+              key={item.key}
+              title={
+                <span>
+                  <Icon type={item.icon} />
+                  <span>{item.title}</span>
+                </span>
+              }
+            >
+              {this.getMenuNodes(item.children)}
+            </SubMenu>
+          ))
+        }
+      }
+
+      return pre
+    }, [])
+  }
+
+  /*
+  在第一次render()之前执行一次
+  为第一个render()准备数据(必须同步的)
+   */
+  componentWillMount() {
+    this.menuNodes = this.getMenuNodes(menuList)
+  }
+
   render() {
-    const path = this.props.location.pathname;
-    const openKey = this.openKey;
+    // debugger
+    // 得到当前请求的路由路径
+    let path = this.props.location.pathname
+    console.log('render()', path)
+    if (path.indexOf('/product') === 0) { // 当前请求的是商品或其子路由界面
+      path = '/product'
+    }
+
+    // 得到需要打开菜单项的key
+    const openKey = this.openKey
+
     return (
       <div className="left-nav">
-        <Link to="/" className="left-nav-header">
-          <img src={logo}></img>
+        <Link to='/' className="left-nav-header">
+          <img src={logo} alt="logo" />
           <h1>硅谷后台</h1>
         </Link>
+
         <Menu
           mode="inline"
           theme="dark"
-          //刷新后高亮之前选择的
           selectedKeys={[path]}
-          //刷新后选择之前选择的
           defaultOpenKeys={[openKey]}
         >
-          {this.menuNodes}
+
+          {
+            this.menuNodes
+          }
+
         </Menu>
       </div>
-    );
+    )
   }
 }
 
 /*
-withRouter: 高阶组件: 包装非路由组件返回一个包装后的新组件, 新组件会向被包装组件传递
-history/location/match 属性
-*/
-export default withRouter(LeftNav);
+withRouter高阶组件:
+包装非路由组件, 返回一个新的组件
+新的组件向非路由组件传递3个属性: history/location/match
+ */
+export default withRouter(LeftNav)
